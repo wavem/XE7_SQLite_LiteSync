b02ce8a24bea791c3d70d303e1d58960c4df7a48
WAVE <fenix24@naver.com>
2021-08-23 10:26:59 +0900

    [New] memo component
    - Default Functions Setting.

--- Sqlite_LiteSync_Sample/Main.cpp
+++ Sqlite_LiteSync_Sample/Main.cpp
@@ -6,11 +6,36 @@
 #include "Main.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+#pragma link "AdvMemo"
 #pragma resource "*.dfm"
 TFormMain *FormMain;
 //---------------------------------------------------------------------------
 __fastcall TFormMain::TFormMain(TComponent* Owner)
 	: TForm(Owner)
 {
+	InitProgram();
 }
 //---------------------------------------------------------------------------
+
+void __fastcall TFormMain::FormClose(TObject *Sender, TCloseAction &Action)
+{
+	ExitProgram();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::InitProgram() {
+	PrintMsg(L"Init Success");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::ExitProgram() {
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::PrintMsg(UnicodeString _str) {
+	int t_RowIdx = memo->Lines->Add(_str);
+    memo->SetCursor(0, t_RowIdx);
+}
+//---------------------------------------------------------------------------
+
--- Sqlite_LiteSync_Sample/Main.h
+++ Sqlite_LiteSync_Sample/Main.h
@@ -7,13 +7,25 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include "AdvMemo.hpp"
 //---------------------------------------------------------------------------
 class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
+	TPanel *___pnBase;
+	TAdvMemo *memo;
+	void __fastcall FormClose(TObject *Sender, TCloseAction &Action);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFormMain(TComponent* Owner);
+
+public: // BASIC MEMBER VARIABLES
+
+public: // BASIC MEMBER FUNCTIONS
+	void __fastcall InitProgram();
+    void __fastcall ExitProgram();
+    void __fastcall PrintMsg(UnicodeString _str);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TFormMain *FormMain;
